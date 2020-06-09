8e8386e3e706a330c63643922a19ee4420938cd0
Stas Dombrovskyi <727727stas@gmail.com>
2020-06-09 22:08:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <ctime>
-#include "DLCircularList.h"
 
 using namespace std;
 
 int main() {
     srand(time(0));
-    DLCircularList<int> D;
-    D.generate(2);
-    D.display();
+    
     return 0;
 }
